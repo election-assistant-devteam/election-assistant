c207e73968ab0512423cea9168a43a1d28c2b307
Chris Ahn <chrisahn1007@gmail.com>
2025-01-15 01:33:08 +0900

    register and login page error msg

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./styles/login.module.scss";
 import Title from "@/components/common/title/Title";
 import Button from "@/components/common/button/Button";
@@ -9,17 +9,56 @@ function index() {
   const [id, setId] = useState<string>();
   const [pw, setPw] = useState<string>();
   const navigate = useNavigate();
+  const [idValidation, setIdValidation] = useState<boolean>(true);
+  const [pwValidation, setPwValidation] = useState<boolean>(true);
+
+  const idFormatCheck = (id: string) => {
+    const regex = /^[a-zA-Z0-9]{4,12}$/;
+    if (!regex.test(id)) {
+      setIdValidation(false);
+    } else {
+      setIdValidation(true);
+    }
+    return;
+  };
+
+  const pwFormatCheck = (pw: string) => {
+    const regex = /^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{9,12}$/;
+    if (!regex.test(pw)) {
+      setPwValidation(false);
+    } else {
+      setPwValidation(true);
+    }
+    return;
+  };
+
+  // useEffect(() => {
+
+  // }, [idValidation, pwValidation]);
 
   const handleLogin = async () => {
+    if (id === undefined || pw === undefined) {
+      alert("아이디나 패스워드를 입력하세요!");
+      return;
+    }
+    idFormatCheck(id);
+    pwFormatCheck(pw);
+    const isIdValid = /^[a-zA-Z0-9]{4,12}$/.test(id); // id 유효성 체크
+    const isPwValid = /^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{9,12}$/.test(pw); // pw 유효성 체크
+
+    if (!isIdValid || !isPwValid) {
+      return;
+    }
+
     const response = await fetch("https://d282ffdd-b1e5-4e5a-bebc-2a161c592cb5.mock.pstmn.io/login/success", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        body: JSON.stringify({
-          id: id,
-          pw: pw,
-        }),
       },
+      body: JSON.stringify({
+        id: id,
+        pw: pw,
+      }),
     });
     const result = await response.json();
 
@@ -41,8 +80,10 @@ function index() {
         <div className={styles.page__contents__loginBox}>
           <div className={styles.page__contents__loginBox__label}>아이디</div>
           <InputBox placeHolder={"아이디를 입력하세요"} handleData={setId}></InputBox>
+          <div className={idValidation ? styles.noErrorMsg : styles.errorMsg}>아이디 형식이 올바르지 않습니다 (4~12자 영문자 또는 숫자)</div>
           <div className={styles.page__contents__loginBox__label}>비밀번호</div>
           <InputBox placeHolder={"비밀번호를 입력하세요"} handleData={setPw}></InputBox>
+          <div className={pwValidation ? styles.noErrorMsg : styles.errorMsg}>비밀번호 형식이 올바르지 않습니다 (9~12자 영문자, 숫자, 특수문자 조합)</div>
           <div className={styles.page__contents__loginBox__button}>
             <Button text={"로그인"} data={[id, pw]} onClick={handleLogin}></Button>
             <Link to={"/register"}>
--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./styles/register.module.scss";
 import NavBar from "@/components/common/navigation/NavBar";
 import InputBox from "@/components/common/input/InputBox";
@@ -7,14 +7,69 @@ import RegisterSuccess from "./registersuccess";
 
 function index() {
   const [email, setEmail] = useState<string>();
+  const [nickname, setNickname] = useState<string>();
   const [id, setId] = useState<string>();
   const [pw, setPw] = useState<string>();
-  const [pwCheck, setPwCheck] = useState<string>();
+  const [pwRepeat, setPwRepeat] = useState<string>();
   const [success, setSuccess] = useState<boolean>(false);
+  const [idValidation, setIdValidation] = useState<boolean>(true);
+  const [pwValidation, setPwValidation] = useState<boolean>(true);
+  const [emailValidation, setEmailValidation] = useState<boolean>(true);
+  const [pwRepeatValidation, setPwRepeatValidation] = useState<boolean>(true);
+
+  const idFormatCheck = (id: string) => {
+    const regex = /^[a-zA-Z0-9]{4,12}$/;
+    if (!regex.test(id)) {
+      setIdValidation(false);
+    } else {
+      setIdValidation(true);
+    }
+    return;
+  };
+
+  const pwFormatCheck = (pw: string) => {
+    const regex = /^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{9,12}$/;
+    if (!regex.test(pw)) {
+      setPwValidation(false);
+    } else {
+      setPwValidation(true);
+    }
+    return;
+  };
+
+  const emailFormatCheck = (email: string) => {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!regex.test(email)) {
+      setEmailValidation(false);
+    } else {
+      setEmailValidation(true);
+    }
+    return;
+  };
+
+  const pwRepeatCheck = (pw: string, pwRepeat: string) => {
+    if (pw !== pwRepeat) {
+      setPwRepeatValidation(false);
+    } else {
+      setPwRepeatValidation(true);
+    }
+  };
 
   const handleRegister = async () => {
-    if (pw !== pwCheck) {
-      alert("비밀번호가 일치하지 않습니다.");
+    if (id === undefined || pw === undefined || email === undefined || nickname === undefined || pwRepeat === undefined) {
+      alert("모든 항목에 대해 입력하세요!");
+      return;
+    }
+    emailFormatCheck(email);
+    idFormatCheck(id);
+    pwFormatCheck(pw);
+    pwRepeatCheck(pw, pwRepeat);
+
+    const isEmailValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+    const isIdValid = /^[a-zA-Z0-9]{4,12}$/.test(id);
+    const isPwValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(pw);
+    const isPwRepeatValid = pw === pwRepeat ? true : false;
+    if (!isEmailValid || !isIdValid || !isPwValid || !isPwRepeatValid) {
       return;
     }
 
@@ -56,18 +111,26 @@ function index() {
             <div className={styles.page__contents__inputBox__emailBox}>
               <div className={styles.label}>이메일</div>
               <InputBox placeHolder={"이메일을 입력하세요"} handleData={setEmail} />
+              <div className={emailValidation ? styles.noErrorMsg : styles.errorMsg}>이메일 형식이 올바르지 않습니다</div>
+            </div>
+            <div className={styles.page__contents__inputBox__nicknameBox}>
+              <div className={styles.label}>닉네임</div>
+              <InputBox placeHolder={"닉네임을 입력하세요"} handleData={setNickname} />
             </div>
             <div className={styles.page__contents__inputBox__idBox}>
               <div className={styles.label}>아이디</div>
               <InputBox placeHolder={"아이디를 입력하세요"} handleData={setId} />
+              <div className={idValidation ? styles.noErrorMsg : styles.errorMsg}>아이디 형식이 올바르지 않습니다 (4~12자 영문자 또는 숫자)</div>
             </div>
             <div className={styles.page__contents__inputBox__pwBox}>
               <div className={styles.label}>비밀번호</div>
               <InputBox placeHolder={"비빈번호를 입력하세요"} handleData={setPw} />
+              <div className={pwValidation ? styles.noErrorMsg : styles.errorMsg}>비밀번호 형식이 올바르지 않습니다 (9~12자 영문자, 숫자, 특수문자 조합)</div>
             </div>
             <div className={styles.page__contents__inputBox__pwCheckBox}>
               <div className={styles.label}>비밀번호 확인</div>
-              <InputBox placeHolder={"비밀번호를 다시 한번 입력하세요"} handleData={setPwCheck} />
+              <InputBox placeHolder={"비밀번호를 다시 한번 입력하세요"} handleData={setPwRepeat} />
+              <div className={pwRepeatValidation ? styles.noErrorMsg : styles.errorMsg}>비밀번호와 같지 않습니다</div>
             </div>
           </div>
           <div className={styles.page__contents__buttonBox}>
