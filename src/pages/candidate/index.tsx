621b9296a06528bb6910554d90043b22de1ab3cc
Chris Ahn <chrisahn1007@gmail.com>
2025-01-24 13:49:55 +0900

    후보자 상세보기 페이지 작업완료

--- src/pages/candidate/index.tsx
+++ src/pages/candidate/index.tsx
@@ -8,21 +8,13 @@ function index() {
   const navigate = useNavigate();
   const [eventName, setEventName] = useState(""); // 상태로 eventName 관리
   const [candidateList, setCandidateList] = useState([]); // 상태로 candidateList 관리
+  const sample = 22;
 
   useEffect(() => {
     const getCandidateList = async () => {
-      const response = await fetch(
-        "https://d282ffdd-b1e5-4e5a-bebc-2a161c592cb5.mock.pstmn.io/candidate/16",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            id: params,
-          }),
-        }
-      );
+      const response = await fetch(`https://d282ffdd-b1e5-4e5a-bebc-2a161c592cb5.mock.pstmn.io/candidate/${params.id}`, {
+        method: "GET",
+      });
 
       if (response.status === 200) {
         const data = await response.json();
@@ -33,44 +25,16 @@ function index() {
     getCandidateList();
   }, []);
 
-  const goToSpec = async (polId) => {
-    const response = await fetch(
-      "https://d282ffdd-b1e5-4e5a-bebc-2a161c592cb5.mock.pstmn.io/politician",
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          id: polId,
-        }),
-      }
-    );
-
-    if (response.status === 200) {
-      const result = await response.json();
-      navigate(`/politician/${result.data.id}`);
-    }
-  };
-
   return (
     <div className={styles.page}>
       <div className={styles.page__contents}>
-        <NavBar text={eventName}></NavBar>
+        <NavBar text={`${eventName} 후보자 목록`}></NavBar>
         <div className={styles.page__contents__cardList}>
           {candidateList.map((event, index) => (
-            <div
-              className={styles.page__contents__cardList__card}
-              key={index}
-              onClick={() => goToSpec(event.id)}
-            >
+            <div className={styles.page__contents__cardList__card} key={index} onClick={() => navigate(`/politician/${event.id}`)}>
               <div className={styles.page__contents__cardList__card__info}>
-                <div className={styles.page__contents__cardList__card__info__party}>
-                  {event.party}
-                </div>
-                <div className={styles.page__contents__cardList__card__info__name}>
-                  {event.name}
-                </div>
+                <div className={styles.page__contents__cardList__card__info__party}>{event.party}</div>
+                <div className={styles.page__contents__cardList__card__info__name}>{event.name}</div>
               </div>
               <div className={styles.page__contents__cardList__card__image}>{event.imageUrl}</div>
             </div>
