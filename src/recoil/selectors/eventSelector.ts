3891efb433d8ab8a45601bba73d55f653b1cde73
Chris Ahn <chrisahn1007@gmail.com>
2025-05-26 21:15:31 +0900

    fix : access_token 유무에 따라 캘린더 엔드포인트 api 호출시 Authorization 헤더를 탈부착

--- src/recoil/selectors/eventSelector.ts
+++ src/recoil/selectors/eventSelector.ts
@@ -2,14 +2,14 @@ import { selector } from "recoil";
 import { yearState } from "../atoms/year";
 
 //"https://d282ffdd-b1e5-4e5a-bebc-2a161c592cb5.mock.pstmn.io/calendar/schedules?year=${yearValue}"
-// `http://54.180.165.220/api/calendar/schedules?userId=${userId}&year=${yearValue}`
+// `http://localhost:9001/calendar/schedules?userId=${userId}&year=${yearValue}`
 const userId = sessionStorage.getItem("id");
 
 export const eventData = selector({
   key: "eventData",
   get: async ({ get }) => {
     const yearValue = get(yearState);
-    const ENDPOINT = `http://localhost:9001/api/calendar/schedules?userId=${userId}&year=${yearValue}`;
+    const ENDPOINT = `http://54.180.165.220/api/calendar/schedules?userId=${userId}&year=${yearValue}`;
     const access_token = sessionStorage.getItem("access-token");
 
     const headers = {
@@ -18,13 +18,16 @@ export const eventData = selector({
     };
 
     try {
-      const res = await fetch(ENDPOINT, {
+      const response = await fetch(ENDPOINT, {
         method: "GET",
         headers,
       });
-      if (res.status === 200) {
-        const result = await res.json();
+      const result = await response.json();
+      if (result.code === 20000) {
+        console.log(result);
         return result.data.calendar;
+      } else {
+        console.log(result.message);
       }
     } catch (e) {
       console.error(e);
