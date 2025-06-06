0aa44cbd670aaa1bd124ee134a9212ffc9f22c86
Chris Ahn <chrisahn1007@gmail.com>
2025-06-06 22:18:42 +0900

    refactor : EventTagBox 컴포넌트로 분리

---
+++ src/pages/calendar/EventTagBox.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+interface Props {
+  eventName: string[];
+}
+
+const EventTagBox = ({ eventName }: Props) => {
+  return (
+    <div className="eventTagBox">
+      {eventName.map((item, index) => (
+        <div key={index} className="eventTag">
+          {item}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default EventTagBox;
