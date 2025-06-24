f51cadb7f29fcb69a7d01efe1f36fbf97341c69d
Chris Ahn <chrisahn1007@gmail.com>
2025-06-25 00:07:03 +0900

    fix : 수정된 백엔드 news api 로직에 맞춰서 LiveNewsSection 컴포넌트 수정

--- src/pages/main/centerview/LiveNewsSection.tsx
+++ src/pages/main/centerview/LiveNewsSection.tsx
@@ -1,8 +1,10 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./LiveNewsSection.module.scss";
 import { MdArrowForwardIos } from "react-icons/md";
 import { useNavigate } from "react-router-dom";
 import LiveNewsItem from "./LiveNewsItem";
+import { apiCall } from "@/services/authServices";
+import Loading from "@/components/common/loading/Loading";
 
 export type NewsItemType = {
   title: string;
@@ -13,16 +15,15 @@ export type NewsItemType = {
 const LiveNewsSection = () => {
   const navigate = useNavigate();
   const [newsData, setNewsData] = useState<NewsItemType[]>(null);
+  const localUrl = "http://localhost:9001/news";
+  const deployUrl = "http://54.180.165.220/api/news";
 
   useEffect(() => {
-    fetch("/news_data.json")
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        setNewsData(data);
-      })
-      .catch((error) => console.error("Error loading news_data", error));
+    const getNews = async () => {
+      const response = await apiCall(deployUrl, "GET");
+      setNewsData(response.data);
+    };
+    getNews();
   }, []);
 
   return (
@@ -31,13 +32,26 @@ const LiveNewsSection = () => {
         <div className={styles.newsContainer__head__title}>실시간 뉴스</div>
       </div>
       <div className={styles.newsContainer__body}>
-        {newsData && newsData.length > 0 && <LiveNewsItem newsData={newsData[0]} />}
-        {newsData && newsData.length > 0 && <LiveNewsItem additional={true} newsData={newsData[1]} />}
-        {newsData && newsData.length > 0 && <LiveNewsItem additional={true} newsData={newsData[2]} />}
+        {newsData ? (
+          <>
+            {newsData && newsData.length > 0 && <LiveNewsItem newsData={newsData[0]} />}
+            {newsData && newsData.length > 0 && (
+              <LiveNewsItem additional={true} newsData={newsData[1]} />
+            )}
+            {newsData && newsData.length > 0 && (
+              <LiveNewsItem additional={true} newsData={newsData[2]} />
+            )}
+          </>
+        ) : (
+          <Loading />
+        )}
       </div>
 
       <div className={styles.newsContainer__foot}>
-        <div className={styles.newsContainer__foot__text} onClick={() => navigate("/news", { state: newsData })}>
+        <div
+          className={styles.newsContainer__foot__text}
+          onClick={() => navigate("/news", { state: newsData })}
+        >
           더 많은 뉴스 보기
         </div>
         <div className={styles.newsContainer__foot__icon}>
