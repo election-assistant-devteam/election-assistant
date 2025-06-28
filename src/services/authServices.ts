e02a3d845185e7fa7c74f86f265974e50a99489e
Chris Ahn <chrisahn1007@gmail.com>
2025-06-28 16:53:30 +0900

    fix :  eventSelector의 api 호출 로직 수정

--- src/services/authServices.ts
+++ src/services/authServices.ts
@@ -1,10 +1,12 @@
 export const API_BASE = import.meta.env.VITE_API_BASE_URL;
 
-export async function apiCall(path: string, method: string, data?) {
+export async function apiCall(path: string, method: string, data?, extra?: boolean) {
+  const access_token = sessionStorage.getItem("access-token");
   const response = await fetch(`${API_BASE}${path}`, {
     method: method,
     headers: {
       "Content-Type": "application/json",
+      ...(extra && { Authorization: `Bearer ${access_token}` }),
     },
     ...(data && { body: JSON.stringify(data) }),
   });
