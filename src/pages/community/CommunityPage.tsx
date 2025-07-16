8c3a8820c66ffa3cc69b20ad8cd985243f2425c9
Chris Ahn <chrisahn1007@gmail.com>
2025-07-17 01:12:21 +0900

    feat : 커뮤니티 게시글 목록 페이지 관련 컴포넌트 개발 완료

--- src/pages/community/CommunityPage.tsx
+++ src/pages/community/CommunityPage.tsx
@@ -1,12 +1,12 @@
 import styles from "./CommunityPage.module.scss";
-import TopNav from "@/components/common/TopNav/TopNav";
 import HotSectionView from "./hot-section/HotSectionView";
 import FreeSectionView from "./free-section/FreeSectionView";
+import NavBar from "@/components/common/navigation/NavBar";
 
 const CommunityPage = () => {
   return (
     <div className={styles.communityPage}>
-      <TopNav />
+      <NavBar text="커뮤니티" />
       <div className={styles.communityPage__contents}>
         <HotSectionView />
         <FreeSectionView />
