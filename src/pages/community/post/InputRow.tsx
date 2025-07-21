c888aa9fbabbd04355d233eed6a34a4bfd14a1f0
Chris Ahn <chrisahn1007@gmail.com>
2025-07-22 00:54:55 +0900

    feat : 커뮤니티 댓글 작성 관련 컴포넌트 작업 완료

--- src/pages/community/post/InputRow.tsx
+++ src/pages/community/post/InputRow.tsx
@@ -1,13 +1,58 @@
 import React, { useRef, useState } from "react";
 import styles from "./InputRow.module.scss";
 import { PiPaperPlaneTiltBold } from "react-icons/pi";
+import CheckCircle from "@assets/checkCircle.svg?react";
+import { apiCall } from "@/services/authServices";
 
-const InputRow = () => {
+interface Props {
+  postId: number;
+  clickedCommentId: number;
+  onCommentSubmit: () => void;
+}
+
+interface CommentReqData {
+  writerId: number;
+  isAnonymous: boolean;
+  content: string;
+  parentId: number | null;
+}
+
+const InputRow = ({ postId, clickedCommentId, onCommentSubmit }: Props) => {
   const [inputValue, setInputValue] = useState("");
+  const [anonymous, setAnonymous] = useState<boolean>(false);
+  const PATH = `/posts/${postId}/comments`;
 
-  const addComment = async () => {};
+  const addComment = async () => {
+    const data: CommentReqData = {
+      writerId: Number(sessionStorage.getItem("id")),
+      isAnonymous: anonymous,
+      content: inputValue,
+      parentId: clickedCommentId,
+    };
+    const response = await apiCall(PATH, "POST", data, true);
+
+    if (response.code === 20000) {
+      alert("댓글이 성공적으로 작성되었습니다");
+      setInputValue("");
+      onCommentSubmit();
+      console.log(response.data);
+    } else {
+      alert("에러");
+      console.log(response.code);
+    }
+  };
   return (
     <div className={styles.inputRow}>
+      <div
+        className={
+          anonymous ? `${styles.inputRow__anonymous} ${styles.active}` : styles.inputRow__anonymous
+        }
+        onClick={() => setAnonymous((prev) => !prev)}
+      >
+        <CheckCircle />
+        익명
+      </div>
+
       <input
         type="text"
         placeholder="댓글을 입력하세요."
